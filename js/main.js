d985ee9891b2044f6bf5624fc1f8fdea5a6204be
Rabii Luena <luenarabii@gmail.com>
2017-05-19 23:46:20 +0300

    Version 2 of my online portfolio

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-(function(window, document, $) {
+(function(window, document, $){
+	var documentSize = $(document).width();
+	
 
-	$(document).on('click', '.top-nav a', function(event) {
+	$(document).on('click', '.navicon', function(event) {
 		event.preventDefault();
-		/* Act on the event */
-
-		var $that = $(this);
-		$that.parents('.top-nav').find('.nav-li').removeClass('active');
-		$that.parents('.middle-container').find('.contents-container').removeClass('active');
-
-		var $link_active = $that.parents('.nav-li').addClass('active');
-		var $hide_container = $that.parents('.middle-container')
-									.find('.'+$that.attr('a_name'))
-									.addClass('active');
+		$that = $(this);		
+		$that.siblings('ul').slideToggle('fast');
 	});
 
+	// $(document).on('click', '.navbar>ul a', function(event) {
+	// 	event.preventDefault();
+	// 	/* Act on the event */
+		
+	// });
+	
 })(window, document, jQuery);
